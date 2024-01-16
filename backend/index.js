2913031e2af112dbcbda49fd30b410f8f6f1287f
RSA-2048 <137219967+RSA-2048@users.noreply.github.com>
2024-01-16 15:34:56 +0200

    added env file

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,29 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import User from "./models/User.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 app.use(cors()); // define who can access our server
 app.use(express.json()); // for parsing json
 app.use(express.urlencoded({ extended: false })); // mainly for working with from data 
 
+const PORT = process.env.PORT || 8080; // set 8080 for default port name if it does not exist in the env
 
 // routes
-app.post('/addUser', async (req, res) => { // async function because we are interacting with the DB
-    // const user = req.body.user;
-    //const { user } = req.body;
-    const newUser = await User.create(req.body);
-    res.send(newUser);
-})
+// app.post('/addUser', async (req, res) => { // async function because we are interacting with the DB
+//     // const user = req.body.user;
+//     //const { user } = req.body;
+//     const newUser = await User.create(req.body);
+//     res.send(newUser);
+// })
 
-mongoose.connect("mongodb+srv://lezendaniel:K8utPFOheCTc2IJn@cluster0.gxgp3br.mongodb.net/AmazonClone?retryWrites=true&w=majority").then(() => {
-    app.listen(8080, function () {
-        console.log("Listening on 8080");
+mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(() => {
+    app.listen(PORT, function () {
+        console.log("Listening on " + PORT);
     })
 }).catch(err => { console.log(err.message); });
 
