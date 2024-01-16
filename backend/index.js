81d18d4654ccaccf0f0035506fea699f0af79664
RSA-2048 <137219967+RSA-2048@users.noreply.github.com>
2024-01-16 15:01:52 +0200

    adde: product model and connection to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/User.js";
 
 const app = express();
 
@@ -9,8 +11,17 @@ app.use(express.urlencoded({ extended: false })); // mainly for working with fro
 
 
 // routes
-
-app.listen(8080, function () {
-    console.log("Listening on 8080");
+app.post('/addUser', async (req, res) => { // async function because we are interacting with the DB
+    // const user = req.body.user;
+    //const { user } = req.body;
+    const newUser = await User.create(req.body);
+    res.send(newUser);
 })
+
+mongoose.connect("mongodb+srv://lezendaniel:K8utPFOheCTc2IJn@cluster0.gxgp3br.mongodb.net/AmazonClone?retryWrites=true&w=majority").then(() => {
+    app.listen(8080, function () {
+        console.log("Listening on 8080");
+    })
+}).catch(err => { console.log(err.message); });
+
 
