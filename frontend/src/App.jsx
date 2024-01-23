f79c089708cf0feb27d38afe617e12ca9c275005
RSA-2048 <137219967+RSA-2048@users.noreply.github.com>
2024-01-23 11:31:45 +0200

    added toast and sign in functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Title from './Components/Shared/Title.jsx'
+import "react-toastify/dist/ReactToastify.css";
 import Container from 'react-bootstrap/Container'
 import { HomePage } from './Pages/HomePage.jsx'
 import Footer from './Components/Shared/Footer.jsx'
 import Header from './Components/Shared/Header.jsx'
 import SignIn from './Pages/SignIn.jsx'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className='d-flex flex-column side-allPage min-width'>
+        <ToastContainer position="bottom-center" limit={1} />
         <Header />
         <main>
           <Container className='mt-3'>
---
+++ frontend/src/utils.js
@@ -0,0 +1,5 @@
+const getError = (err) => {
+    return err.message && err.response.data.message ? err.response.data.message : err.message
+};
+
+export { getError };
